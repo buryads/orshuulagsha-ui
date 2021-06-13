8d6348d9062dc4db67c0a4e1e6bdbd3ac4df611e
Yurij Karpov <acrossoffwest@gmail.com>
2021-06-13 20:06:19 +0200

    (fix) Fixed layout, added variables for textareas

--- pages/index.vue
+++ pages/index.vue
@@ -8,28 +8,21 @@
         <textarea
           placeholder="Введите текст" name="" id="first" rows="10"
           class="p-5 w-full border-b-2 border-fuchsia-600"
+          v-model="from"
         ></textarea>
       </div>
       <div class="my-5 m-auto text-center">
-        <svg class="inline" style="width: 30px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <svg class="inline cursor-pointer" style="width: 30px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
           <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
         </svg>
       </div>
       <div class="flex-auto">
-        <label for="second"></label>
         <textarea
           placeholder="Мэдуулэл бэшэшгты" name="" id="second" rows="10"
           class="p-5 w-full border-b-2 border-fuchsia-600"
+          v-model="to"
         ></textarea>
       </div>
-    </div>
-    <div class="block mt-20 text-gray-800">
-      <h2>Бурятский язык <sup><a class="text-blue-500" href="https://ru.wikipedia.org/wiki/%D0%91%D1%83%D1%80%D1%8F%D1%82%D1%81%D0%BA%D0%B8%D0%B9_%D1%8F%D0%B7%D1%8B%D0%BA" target="_blank">Википедия</a></sup></h2>
-      <p>
-        Буря́тский язы́к (самоназвание — буряад хэлэн) (в СССР с 1917 по 1956 год — бурят-монгольский язык;
-        в Китае — баргу-бурятский диалект монгольского языка) — язык бурят и баргутов. Один
-        из двух, наряду с русским, официальных языков Республики Бурятия.
-      </p>
     </div>
     <div class="block mt-40 text-gray-500">
       <p>
@@ -43,7 +36,19 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  watch: {
+    from(newVal) {
+      console.log(newVal)
+    }
+  },
+  data () {
+    return {
+      from: '',
+      to: '',
+    }
+  }
+})
 </script>
 
 <style>
