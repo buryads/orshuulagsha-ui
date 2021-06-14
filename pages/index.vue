1a39d1a04df710a7e83e81704609c1758d10451c
Yurij Karpov <acrossoffwest@gmail.com>
2021-06-14 22:34:36 +0200

    (add) Added buttons for send translate request

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="container mx-auto px-10">
     <h1 class="text-2xl sm:text-4xl lg:text-6xl py-10">
-      Бурятско-Русский переводчик
+      Бурятско-Русский словарь
     </h1>
     <div class="block md:flex">
       <div class="flex-auto">
         <textarea
-          placeholder="Мэдуулэл бэшэшгты" name="" id="second" rows="10"
+          placeholder="Угэ бэшэгты" name="" id="second" rows="10"
           class="p-5 w-full border-b-2 border-fuchsia-600"
           v-model="to"
-          @keypress="changeTo"
+          @keypress="changeTo($event.target)"
+          ref="to"
         ></textarea>
+        <button @click="changeTo($refs.to)" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-full">
+          <span class="pr-5">Оршуулха</span>
+          <svg v-if="toLoading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </button>
       </div>
       <div class="my-5 m-auto text-center">
         <svg class="inline cursor-pointer" style="width: 30px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -19,11 +27,19 @@
       </div>
       <div class="flex-auto">
         <textarea
-          placeholder="Введите текст" name="" id="first" rows="10"
+          placeholder="Введите слово" name="" id="first" rows="10"
           class="p-5 w-full border-b-2 border-fuchsia-600"
           v-model="from"
-          @keypress="changeFrom"
+          ref="from"
+          @keypress="changeFrom($event.target)"
         ></textarea>
+        <button @click="changeFrom($refs.from)" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-full">
+          <span class="pr-5">Перевести</span>
+          <svg v-if="fromLoading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </button>
       </div>
     </div>
     <div class="block mt-10 text-gray-500">
@@ -73,9 +89,11 @@ import Vue from 'vue';
 export default Vue.extend({
   data () {
     return {
-      title: 'Бурятско-Русский переводчик',
+      title: 'Бурятско-Русский словарь',
       from: '',
       to: '',
+      fromLoading: false,
+      toLoading: false,
       translateTimeout: null,
       translates: {
         result: [],
@@ -91,7 +109,7 @@ export default Vue.extend({
         {
           hid: 'description',
           name: 'description',
-          content: 'Бурятско-Русский переводчик'
+          content: 'Бурятско-Русский словарь'
         }
       ]
     }
@@ -101,13 +119,16 @@ export default Vue.extend({
       clearTimeout(this.translateTimeout);
 
       this.translateTimeout = setTimeout(() => {
-        const value = e.target.value;
+        this.fromLoading = true;
+        const value = e.value;
         this.$axios.get('/api/translate/ru2bur?' + this.jsonObjectToQueryString({
           word: value
         })).then(({data: {data}}) => {
           this.to = data?.result?.[0]?.translations?.[0]?.name;
           console.log(this.to);
           this.translates = data;
+        }).finally(() => {
+          this.fromLoading = false;
         });
       }, 500);
     },
@@ -115,12 +136,15 @@ export default Vue.extend({
       clearTimeout(this.translateTimeout);
 
       this.translateTimeout = setTimeout(() => {
-        const value = e.target.value;
+        this.toLoading = true;
+        const value = e.value;
         this.$axios.get('/api/translate/bur2ru?' + this.jsonObjectToQueryString({
           word: value
         })).then(({data: {data}}) => {
           this.from = data?.result?.[0]?.translations?.[0]?.name;
           this.translates = data;
+        }).finally(() => {
+          this.toLoading = false;
         });
       }, 500);
     },
