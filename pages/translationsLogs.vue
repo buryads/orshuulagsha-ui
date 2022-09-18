3c14360dedb1aa467a83eb184043a80db9ebb008
Iurii Karpov <acrossoffwest@gmail.com>
2022-09-18 17:20:52 +0200

    fix

--- pages/translationsLogs.vue
+++ pages/translationsLogs.vue
@@ -178,7 +178,7 @@ export default Vue.extend({
     detectDevice (userAgent) {
       const deviceDetector = new DeviceDetector();
       const info = deviceDetector.parse(userAgent);
-      return `${info?.device?.brand}(${info?.device?.type}) -> ${info?.os?.name} -> ${info?.client?.name}`;
+      return info ? `${info?.device?.brand}(${info?.device?.type}) -> ${info?.os?.name} -> ${info?.client?.name}` : userAgent;
     },
     formatDate (date: any) {
       return moment(date).format('MMMM Do YYYY, h:mm:ss a')
