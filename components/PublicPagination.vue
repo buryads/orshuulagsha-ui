05915d9aa9fa584a615d38f2fc1b0b9eabc283f2
Iurii Karpov <y.karpov@dats.team>
2022-12-25 18:10:53 +0100

    fix

--- components/PublicPagination.vue
+++ components/PublicPagination.vue
@@ -1,33 +1,37 @@
 <template>
-  <ul class="flex list-reset border border-grey-light rounded w-auto font-sans">
-    <li v-if="pagination.current_page > 1">
-      <a class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2"
-         href="#"
-         @click.prevent="change(pagination.current_page -1)"
-      >
-        {{ $t('previous') }}
-      </a>
-    </li>
-    <li v-for="page in pages" :key="page" >
-      <a :class="[page == pagination.current_page ? 'text-blue-500 bg-blue border-r border-blue':'hover:text-gray-500 hover:bg-blue text-blue border-r border-grey-light', 'block px-3 py-2']"
-         href="#"
-         @click.stop="change(page)"
-      >
-        {{ page }}
-      </a>
-    </li>
-    <li v-if="pagination.current_page < pagination.last_page">
-      <a class="block hover:text-white hover:bg-blue text-blue px-3 py-2"
-         href="#"
-         @click.prevent="change(pagination.current_page + 1)"
-      >
-        {{ $t('next') }}
-      </a>
-    </li>
-  </ul>
+  <div class="w-full flex justify-center">
+    <ul class="flex list-reset text-base border border-grey-light rounded w-auto font-sans">
+      <li v-if="pagination.current_page > 1">
+        <a class="block hover:text-blue-500 hover:bg-blue text-blue border-r border-grey-light px-3 py-2"
+           href="#"
+           @click.prevent="change(pagination.current_page -1)"
+        >
+          <<
+        </a>
+      </li>
+      <li v-for="page in pages" :key="page" >
+        <a :class="[page == pagination.current_page ? 'text-blue-500 bg-blue border-r border-blue':'hover:text-gray-500 hover:bg-blue text-blue border-r border-grey-light', 'block px-3 py-2']"
+           href="#"
+           @click.stop="change(page)"
+        >
+          {{ page }}
+        </a>
+      </li>
+      <li v-if="pagination.current_page < pagination.last_page">
+        <a class="block hover:text-blue-500 hover:bg-blue text-blue px-3 py-2"
+           href="#"
+           @click.prevent="change(pagination.current_page + 1)"
+        >
+          >>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import UrlHelper from "~/utils/url";
+
 export default {
   props: {
     pagination: {
@@ -48,7 +52,7 @@ export default {
       if (from < 1) {
         from = 1;
       }
-      let to = from + (this.offset * 2);
+      let to = from + (this.offset * 1.2);
       if (to >= this.pagination.last_page) {
         to = this.pagination.last_page;
       }
@@ -61,6 +65,7 @@ export default {
   },
   methods: {
     change: function(page) {
+      UrlHelper.setQueryParameter('page', page);
       this.$emit('paginate', page);
     }
   }
