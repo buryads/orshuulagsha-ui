be0e31c36979f4ee5da09fad43e08dfa70b48328
Iurii Karpov <y.karpov@dats.team>
2022-12-26 12:37:51 +0100

    add overlay to navbar on dropdowns

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,14 +21,12 @@
             <outline-information-circle-icon class="w-8 fas fa-stream p-2 bg-gray-200 rounded-full inline-block" />
             <span class="mx-1 hidden lg:inline">{{ $t('quiz') }}</span>
           </nuxt-link>
-          <nuxt-link :to="localePath('/words')" class="px-2 py-1 cursor-pointer hover:bg-gray-200 hover:text-gray-700 text-sm rounded mb-5">
-
-          </nuxt-link>
           <div class="inline-block cursor-pointer" @click.prevent="showDatabaseDropdown = !showDatabaseDropdown">
             <nuxt-link to="#" class="px-2 py-1 hover:bg-gray-200 hover:text-gray-700 text-sm rounded mb-5">
               <outline-view-list-icon class="w-8 fas fa-stream p-2 bg-gray-200 rounded-full inline-block" />
               <span class="mx-1 hidden lg:inline">{{ $t('words') }}</span>
             </nuxt-link>
+            <div v-if="showDatabaseDropdown" class="bg-blend-lighten md:bg-blend-darken w-screen h-screen fixed top-0 left-0 z-20"></div>
             <div v-bind:class="{'hidden': !showDatabaseDropdown, 'block': showDatabaseDropdown}" class="mt-5 absolute bg-white text-gray-700 text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1" style="min-width:12rem" ref="popoverDropdownRef">
               <nuxt-link :to="localePath('/words/')" class="text-sm py-2 px-4 font-normal hover:bg-gray-200 block w-full whitespace-nowrap bg-transparent ">
                 {{ $t('buryadDictionary') }}
@@ -42,129 +40,83 @@
             <nuxt-link to="#" class="px-2 py-1 hover:bg-gray-200 hover:text-gray-700 text-sm rounded mb-5">
               <img :src="`/flags/${$i18n.locale}.svg`" class="w-6 inline-block -mt-1"/>
             </nuxt-link>
+            <div v-if="dropdownPopoverShow" class="bg-blend-lighten md:bg-blend-darken w-screen h-screen fixed top-0 left-0 z-20"></div>
             <div v-bind:class="{'hidden': !dropdownPopoverShow, 'block': dropdownPopoverShow}" class="mt-5 absolute bg-white text-gray-700 text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1" style="min-width:12rem" ref="popoverDropdownRef">
-              <nuxt-link :to="switchLocalePath('bur')" class="text-sm py-2 px-4 font-normal hover:bg-gray-200 block w-full whitespace-nowrap bg-transparent ">
-                <img :src="`/flags/bur.svg`" class="w-6 inline-block -mt-1"/> {{ $t('bur') }}
-              </nuxt-link>
-              <nuxt-link :to="switchLocalePath('en')" class="text-sm py-2 px-4 font-normal hover:bg-gray-200 block w-full whitespace-nowrap bg-transparent ">
-                <img :src="`/flags/en.svg`" class="w-6 inline-block -mt-1"/> {{ $t('en') }}
-              </nuxt-link>
-              <nuxt-link :to="switchLocalePath('ua')" class="text-sm py-2 px-4 font-normal hover:bg-gray-200 block w-full whitespace-nowrap bg-transparent ">
-                <img :src="`/flags/ua.svg`" class="w-6 inline-block -mt-1"/> {{ $t('ua') }}
-              </nuxt-link>
-              <nuxt-link :to="switchLocalePath('ru')" class="text-sm py-2 px-4 font-normal hover:bg-gray-200 block w-full whitespace-nowrap bg-transparent ">
-                <img :src="`/flags/ru.svg`" class="w-6 inline-block -mt-1"/> {{ $t('ru') }}
+              <nuxt-link v-for="locale in locales" v-if="$i18n.locale !== locale" :key="locale" :to="switchLocalePath(locale)" class="text-sm py-2 px-4 font-normal hover:bg-gray-200 block w-full whitespace-nowrap bg-transparent ">
+                <img :src="`/flags/${locale}.svg`" class="w-6 inline-block -mt-1"/> {{ $t(locale) }}
               </nuxt-link>
             </div>
           </div>
-        </div>
-      </div>
-    </nav>
-
-    <nav v-else class="container mx-auto bg-white w-full flex relative justify-between items-center mx-auto md:px-8 px-3 h-20">
-      <!-- logo -->
-      <div class="inline-flex">
-        <nuxt-link to="/">
-          <nuxt-link class="inline-block py-2 md:px-3 hover:bg-gray-200 rounded-full" to="/">
-            <div class="flex items-center relative cursor-pointer whitespace-nowrap">
-              <outline-book-open-icon class="w-12 h-12"/>
-              <span class="ml-3">{{ locale.index.title }}</span>
-            </div>
+          <nuxt-link :to="localePath('/login')" class="px-2 py-1 cursor-pointer hover:bg-gray-200 hover:text-gray-700 text-sm rounded mb-5">
+            <outline-login-icon class="w-8 fas fa-stream p-2 bg-gray-200 rounded-full inline-block" />
+            <span class="mx-1 hidden lg:inline">{{ $t('login') }}</span>
           </nuxt-link>
-        </nuxt-link>
-      </div>
-
-      <!-- end logo -->
-
-      <!-- login -->
-      <div v-if="user.id" class="flex-initial">
-        <div class="flex justify-end items-center relative">
-          <div class="flex mr-4 items-center">
-            <div class="flex items-center relative cursor-pointer whitespace-nowrap">
-              <outline-beaker-icon class="w-5 h-5 inline lg:hidden" /> <span class="hidden lg:inline">Words Matchers:</span>
-            </div>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/words-matcher/bur/ru">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">Bur>Ru</div>
-            </nuxt-link>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/words-matcher/ru/bur">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">Ru>Bur</div>
-            </nuxt-link>
-            <span class="inline-block py-2 px-3 rounded-full">
-            <div class="flex items-center relative cursor-pointer whitespace-nowrap">|</div>
-          </span>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full whitespace-nowrap" to="/words/bur/ru">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">
-                <outline-document-add-icon class="w-5 h-5" /> <span class="hidden lg:inline">Add new words</span>
-              </div>
-            </nuxt-link>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/logs">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">
-                <outline-document-report-icon class="w-5 h-5" /> <span class="hidden lg:inline">Logs</span>
-              </div>
-            </nuxt-link>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/logs/unique-not-found-words">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">
-                <outline-emoji-sad-icon class="w-5 h-5" />
-              </div>
-            </nuxt-link>
-          </div>
-
-          <div class="block">
-            <div class="inline relative">
-              <div type="button" class="inline-flex items-center relative px-2 rounded-full">
-                <div class="pl-1 hidden lg:inline">
-                  {{ user.name }}({{ user.email }})
+          <div v-if="user.id" class="flex-initial">
+            <div class="flex justify-end items-center relative">
+              <div class="flex mr-4 items-center">
+                <div class="flex items-center relative cursor-pointer whitespace-nowrap">
+                  <outline-beaker-icon class="w-5 h-5 inline lg:hidden" /> <span class="hidden lg:inline">Words Matchers:</span>
                 </div>
+                <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" :to="localePath('/words-matcher/bur/ru')">
+                  <div class="flex items-center relative cursor-pointer whitespace-nowrap">Bur>Ru</div>
+                </nuxt-link>
+                <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" :to="localePath('/words-matcher/ru/bur')">
+                  <div class="flex items-center relative cursor-pointer whitespace-nowrap">Ru>Bur</div>
+                </nuxt-link>
+                <span class="inline-block py-2 px-3 rounded-full">
+              <div class="flex items-center relative cursor-pointer whitespace-nowrap">|</div>
+            </span>
+                <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full whitespace-nowrap" :to="localePath('/words/bur/ru')">
+                  <div class="flex items-center relative cursor-pointer whitespace-nowrap">
+                    <outline-document-add-icon class="w-5 h-5" /> <span class="hidden lg:inline">Add new words</span>
+                  </div>
+                </nuxt-link>
+                <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" :to="localePath('/logs')">
+                  <div class="flex items-center relative cursor-pointer whitespace-nowrap">
+                    <outline-document-report-icon class="w-5 h-5" /> <span class="hidden lg:inline">Logs</span>
+                  </div>
+                </nuxt-link>
+                <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" :to="localePath('/logs/unique-not-found-words')">
+                  <div class="flex items-center relative cursor-pointer whitespace-nowrap">
+                    <outline-emoji-sad-icon class="w-5 h-5" />
+                  </div>
+                </nuxt-link>
+              </div>
 
-                <div class="block flex-grow-0 flex-shrink-0 h-10 w-12 pl-5">
-                  <outline-user-icon class="w-full h-full"/>
+              <div class="block">
+                <div class="inline relative">
+                  <div type="button" class="inline-flex items-center relative px-2 rounded-full">
+                    <div class="pl-1 hidden lg:inline">
+                      {{ user.name }}({{ user.email }})
+                    </div>
+
+                    <div class="block flex-grow-0 flex-shrink-0 h-10 w-12 pl-5">
+                      <outline-user-icon class="w-full h-full"/>
+                    </div>
+
+                    <button @click.prevent="logout" class="ml-5 p-2 border rounded-full hover:shadow-lg">
+                      <outline-logout-icon class="w-5 h-5" />
+                    </button>
+                  </div>
                 </div>
-
-                <button @click.prevent="logout" class="ml-5 p-2 border rounded-full hover:shadow-lg">
-                  <outline-logout-icon class="w-5 h-5" />
-                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div v-else class="flex-initial">
-        <div class="flex justify-end items-center relative">
-          <div class="flex mr-4 items-center">
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/games">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">Games</div>
-            </nuxt-link>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/names">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">Names</div>
-            </nuxt-link>
-            <nuxt-link class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" to="/quiz">
-              <div class="flex items-center relative cursor-pointer whitespace-nowrap">Quiz</div>
-            </nuxt-link>
-          </div>
-          <div class="inline relative">
-            <div type="button" class="inline-flex items-center relative md:px-2 rounded-full">
-              <nuxt-link to="/login" class="md:ml-5 p-2 border rounded-full hover:shadow-lg">
-                <outline-login-icon class="w-5 h-5" />
-              </nuxt-link>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- end login -->
     </nav>
   </div>
 </template>
 
 <script>
-import locales from '../i18n/locales'
-
 export default {
   name: "Navbar",
   data () {
     return {
       user: {},
       showDatabaseDropdown: false,
-      dropdownPopoverShow: false
+      dropdownPopoverShow: false,
+      locales: this.$i18n.locales
     }
   },
   computed: {
