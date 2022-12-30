d3e7220f707d5049517c308d09b03682f60f35e4
Iurii Karpov <acrossoffwest@gmail.com>
2022-12-30 10:58:26 +0100

    refactoring and add clients only part in to pack form page

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header id="header" class="relative z-20">
+    <div>
+      <div class="flex items-center">
+        <h1 class="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">
+          <slot></slot>
+        </h1>
+        <slot name="controls"></slot>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/packFormPage.vue
+++ pages/packFormPage.vue
@@ -1,65 +1,47 @@
 <template>
   <div class="container mx-auto px-10 pb-10">
+    <client-only>
     <div class="block md:flex">
       <div class="flex-auto">
         <div class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
           <header id="header" class="relative z-20">
-            <div>
-              <p class="text-red-500" v-if="error.message">{{ $t(error.message) }}</p>
-              <div class="flex items-center w-full">
-                <Input id="packName" :placeholder="$t('enterPackName')" type="text" v-model="pack.name" class="w-full inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200"/>
-              </div>
-              <textarea id="packDescription" :placeholder="$t('enterPackDescription')" rows="10" v-model="pack.description" class="w-full p-5 inline-block text-base font-extrabold text-slate-900 tracking-tight dark:text-slate-200"/>
-              <div class="form-check">
-                <input type="checkbox" id="flexCheckChecked" v-model="pack.is_private">
-                <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
-                  Is private
-                </label>
-              </div>
+            <p class="text-red-500" v-if="error.message">{{ $t(error.message) }}</p>
+            <Input id="packName" :placeholder="$t('enterPackName')" type="text" v-model="pack.name" class="w-full bg-gray-200 border-none focus:border-none inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200"/>
+            <textarea id="packDescription" :placeholder="$t('enterPackDescription')" rows="3" v-model="pack.description" class="w-full bg-gray-200 border-none focus:border-none p-5 inline-block text-base font-extrabold text-slate-900 tracking-tight dark:text-slate-200"></textarea>
+            <div class="form-check">
+              <input type="checkbox" id="flexCheckChecked" v-model="pack.is_private">
+              <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
+                Is private
+              </label>
             </div>
           </header>
-          <div class="mt-10 relative">
-            <div class="overflow-hidden w-full lg:overflow-auto scrollbar:!w-1.5 scrollbar:!h-1.5 scrollbar:bg-transparent scrollbar-track:!bg-slate-100 scrollbar-thumb:!rounded scrollbar-thumb:!bg-slate-300 scrollbar-track:!rounded dark:scrollbar-track:!bg-slate-500/[0.16] dark:scrollbar-thumb:!bg-slate-500/50">
-              <div class="grid grid-flow-row-dense grid-cols-1">
-                <div class="rounded overflow-hidden bg-gray-200 mx-2 col-span-1" v-if="burWords.length > 10">
-                  <div class="px-6 py-4">
-                    <span class="grid grid-flow-row-dense grid-cols-12">
-                      <span class="col-span-1 inline-block"></span>
-                      <span class="col-span-9 inline-block text-left">
-                        <Button :label="$t('addBurword')" class="lg:w-full" @click="addBurword()"/>
-                      </span>
-                      <span class="inline-block col-span-2" ></span>
-                    </span>
-                  </div>
-                  <hr>
-                </div>
-                <div v-for="burword in burWords" class="rounded overflow-hidden bg-gray-200 mx-2 col-span-1">
-                  <div class="px-6 py-4">
-                    <p class="grid grid-flow-row-dense grid-cols-12">
-                      <Speech v-if="burword.speechs && burword.speechs.length" :speech="burword.speechs[0]"/>
-                      <span class="col-span-9 inline-block text-left">
-                        <span>{{ burword.name }} <a href="#" v-if="pack.user_id === $auth.user.id || $authUtils().isUserA('admin')" @click="removeBurword(burword)"><outline-trash-icon class="cursor-pointer w-5 h-5 hover:bg-gray-500 rounded inline-block" /></a></span>
-                        <br>
-                        <span class="text-gray-700" v-if="burword.translations">{{ burword.translations.map(v => v.name)[0] }}</span>
-                      </span>
-                      <img v-if="burword.images && burword.images.length" class="inline-block col-span-2" width="50" height="25" :src="burword.images[0].url" alt="Sunset in the mountains"></p>
-                  </div>
-                  <hr>
-                </div>
-                <div v-if="isEdit" class="rounded overflow-hidden bg-gray-200 mx-2 col-span-1">
-                  <div class="px-6 py-4">
-
-                  </div>
-                  <hr>
-                </div>
+          <div class="grid grid-flow-row-dense grid-cols-1">
+            <div v-for="burword in burWords" class="rounded overflow-hidden bg-gray-200 mx-2 col-span-1">
+              <div class="px-6 py-4">
                 <div class="grid grid-flow-row-dense grid-cols-12">
-                      <div class="col-span-8 inline-block">
-                        <Button :label="isEdit ? $t('save') : $t('create')" class="lg:w-full" @click="save()"/>
-                      </div>
-                      <div class="col-span-4 inline-block text-left pl-5">
-                        <Button :label="$t('addBurword')" class="lg:w-full" @click="showAddBurword = true"/>
-                      </div>
-                    </div>
+                  <Speech v-if="burword.speechs && burword.speechs.length" :speech="burword.speechs[0]"/>
+                  <div class="col-span-9 text-left">
+                    <p>
+                      {{ burword.name }}
+                      <a href="#" v-if="pack.user_id === $auth.user.id || $authUtils().isUserA('admin')" @click="removeBurword(burword)"><outline-trash-icon class="cursor-pointer w-5 h-5 hover:bg-gray-500 rounded inline-block" /></a>
+                    </p>
+                    <p class="text-gray-700" v-if="burword.translations">
+                      {{ burword.translations.map(v => v.name)[0] }}
+                    </p>
+                  </div>
+                  <div class="col-span-2">
+                    <img v-if="burword.images && burword.images.length" width="50" height="25" :src="burword.images[0].url">
+                  </div>
+                </div>
+              </div>
+              <hr>
+            </div>
+            <div class="grid grid-flow-row-dense grid-cols-12">
+              <div class="col-span-8">
+                <Button :label="isEdit ? $t('save') : $t('create')" class="lg:w-full" @click="save()"/>
+              </div>
+              <div class="col-span-4 text-left pl-5">
+                <Button :label="$t('addBurword')" class="lg:w-full" @click="showAddBurword = true"/>
               </div>
             </div>
           </div>
@@ -69,17 +51,23 @@
     <Modal className="w-8/12" v-show="showAddBurword">
       <template v-slot:header>Add new word</template>
       <template v-slot:body>
-        <div>
-          <Input id="burWord" :placeholder="$t('enterBurWord')" type="text" v-model="burWordSearch" class="lg:w-full inline-block text-base font-extrabold text-slate-900 tracking-tight dark:text-slate-200"/>
-        </div>
+        <Input id="burWord" :placeholder="$t('enterBurWord')" type="text" v-model="burWordSearch" class="lg:w-full inline-block text-base font-extrabold text-slate-900 tracking-tight dark:text-slate-200"/>
         <div :key="update">
-          <div v-for="burWord in burWordSearchResults"><p>{{ burWord.name }} <span @click="addBurword(burWord)" class="cursor-pointer"><outline-plus-icon class="w-5 hover:bg-gray-500 rounded inline-block"/></span></p></div>
+          <div v-for="burWord in burWordSearchResults">
+            <p>
+              {{ burWord.name }}
+              <span @click="addBurword(burWord)" class="cursor-pointer">
+                <outline-plus-icon class="w-5 hover:bg-gray-500 rounded inline-block"/>
+              </span>
+            </p>
+          </div>
         </div>
       </template>
       <template v-slot:footer>
         <Button :label="$t('close')" class="lg:w-full" @click="showAddBurword = false"/>
       </template>
     </Modal>
+    </client-only>
   </div>
 </template>
 
