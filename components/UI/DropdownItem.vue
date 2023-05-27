d343e5d4f4e88b62aeeafcc9fa7348cac4f0347f
savayer <sava0094@gmail.com>
2023-05-27 21:54:09 +0800

    (wip) lang switcher. The plugin is not ready and the docs is not relevant

---
+++ components/UI/DropdownItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <MenuItem v-slot="{ active }">
+    <a
+      href="#"
+      :class="[
+        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+        'block px-4 py-2 text-sm',
+      ]"
+    >
+      <slot />
+    </a>
+  </MenuItem>
+</template>
+
+<script setup lang="ts">
+  import { MenuItem } from '@headlessui/vue';
+</script>
