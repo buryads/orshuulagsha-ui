28fa6f99436b9407a32e086561070f78e49d6fa6
savayer <sava0094@gmail.com>
2023-05-28 11:02:16 +0800

    created LinkCard, Arrow components. Implemented games page

---
+++ components/UI/LinkCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    :class="[
+      index === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+      index === 1 ? 'sm:rounded-tr-lg' : '',
+      'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500',
+    ]"
+  >
+    <div class="mt-8">
+      <h3 class="text-xl font-semibold leading-6 text-gray-900">
+        <a :href="link" class="focus:outline-none">
+          <span class="absolute inset-0" aria-hidden="true" />
+          {{ title }}
+        </a>
+      </h3>
+
+      <p class="mt-2 text-sm text-gray-500">
+        {{ text }}
+      </p>
+    </div>
+    <span
+      class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+      aria-hidden="true"
+    >
+      <IconsArrow class="h-6 w-6" />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    index: number;
+    link: string;
+    title: string;
+    text: string;
+  }
+
+  const props = defineProps<Props>();
+</script>
--- pages/games.vue
+++ pages/games.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="container">
-    {{ $t('games') }}
+  <div class="container mt-4 text-gray-600">
+    <h1 class="text-xl font-bold text-gray-900 md:text-3xl">
+      {{ $t('games') }}
+    </h1>
+
+    <p class="prose mt-2">{{ $t('localizedForBuryadLanguageGames') }}</p>
+
+    <div
+      class="mt-5 divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0"
+    >
+      <LinkCard
+        v-for="(game, index) in games"
+        :key="game.title"
+        :class="[
+          index === games.length - 2 ? 'sm:rounded-bl-lg' : '',
+          index === games.length - 1
+            ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none'
+            : '',
+        ]"
+        :title="game.title"
+        :link="game.link"
+        :index="+index"
+        :text="game.description"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import games from '../constants/games.json';
   import { useI18n } from 'vue-i18n';
+  import LinkCard from '~/components/UI/LinkCard.vue';
 
   const { t } = useI18n();
 
@@ -22,6 +46,11 @@
         property: 'title',
         name: 'title',
         content: t('games'),
+      },
+      {
+        property: 'og:description',
+        name: 'description',
+        content: t('localizedForBuryadLanguageGames'),
       },
     ],
   });
