f2ba7f38600ca82785dadab0a6b9cc86bac25409
savayer <sava0094@gmail.com>
2023-06-01 11:16:44 +0800

    created BuryadInput with the buryad letters. Used this component instead of usual input

--- components/UI/Input.vue
+++ components/UI/Input.vue
@@ -4,21 +4,23 @@
     :name="name"
     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
     :value="modelValue"
+    :placeholder="placeholder"
     @keydown="keyDown"
     @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
 
 <script setup lang="ts">
-  interface Props {
+  export interface InputProps {
     type?: string;
     name?: string;
+    placeholder?: string;
     modelValue?: string;
   }
 
   const emit = defineEmits(['update:modelValue', 'change']);
 
-  const props = withDefaults(defineProps<Props>(), {
+  const props = withDefaults(defineProps<InputProps>(), {
     type: 'text',
     name: 'input',
   });
