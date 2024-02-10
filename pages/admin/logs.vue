8351edad78530dbcab38ef7489d5ab325ba35c70
savayer <sava0094@gmail.com>
2024-02-10 13:44:26 +0400

    finished with logs page

--- pages/admin/logs.vue
+++ pages/admin/logs.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="container mt-6">
     <div class="sm:flex sm:items-center">
-      <div class="sm:flex-auto">
+      <div class="text-gray-700 sm:flex-auto">
         <h1 class="title">Translation logs</h1>
-        <p class="mt-2 text-sm text-gray-700">List of attempts to translate.</p>
+        <p class="mt-2 text-sm">List of attempts to translate.</p>
+        <ul class="mt-2 text-sm">
+          <li>
+            All translations: {{ allTranslationsNumber.toLocaleString() }}
+          </li>
+          <li>
+            Ru to Bur translations:
+            {{ ru2burTranslationsNumber.toLocaleString() }}
+          </li>
+          <li>
+            Bur to Ru translations:
+            {{ bur2ruTranslationsNumber.toLocaleString() }}
+          </li>
+        </ul>
       </div>
     </div>
 
-    <div class="mt-8 flow-root">
+    <div class="mt-8 flow-root overflow-x-auto">
       <table class="min-w-full border-separate border-spacing-0" v-if="logs">
         <thead>
           <tr>
             <th
               scope="col"
-              class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+              class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
             >
               Word / Phrase
             </th>
             <th
               scope="col"
-              class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+              class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
             >
               Results
             </th>
@@ -66,7 +79,7 @@
             <td
               :class="[
                 index !== logs.length - 1 ? 'border-b border-gray-200' : '',
-                'hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell',
+                'whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell',
               ]"
             >
               {{ item.results_count }}
@@ -74,7 +87,7 @@
             <td
               :class="[
                 index !== logs.length - 1 ? 'border-b border-gray-200' : '',
-                'hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell',
+                'whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell',
               ]"
             >
               {{
@@ -116,6 +129,15 @@
           </tr>
         </tbody>
       </table>
+
+      <UIPagination
+        v-if="allTranslationsNumber / PER_PAGE"
+        class="mt-6"
+        :page="currentPage"
+        :per-page="PER_PAGE"
+        :last-page="lastPage"
+        @changePage="changePage"
+      />
     </div>
   </div>
 </template>
@@ -127,20 +149,62 @@
   import { TrashIcon } from '@heroicons/vue/24/outline';
   import detectDevice from '../../utils/detectDevice';
   import dateFormat from 'dateformat/lib/dateformat';
+  import type { metaResponse } from '~/repository/modules/types';
 
   useHead({
     title: 'Logs',
   });
 
-  const { $api } = useNuxtApp();
-  const logs: Ref<TranslationLog[] | null> = ref([]);
-  const { data } = await useAsyncData('admin-logs', () =>
-    $api.admin.getTranslationLogs(),
-  );
+  let allTranslationsNumber = 0;
+  let ru2burTranslationsNumber = 0;
+  let bur2ruTranslationsNumber = 0;
 
-  logs.value = data.value;
+  const { $api } = useNuxtApp();
+  const route = useRoute();
+  const logs: Ref<TranslationLog[] | null> = ref([]);
+  const isLoading = ref(false);
+  const currentPage = ref(Number(route.query.page) || 1);
+  const PER_PAGE = 100;
+
+  const { data } = await useAsyncData('admin-logs', () =>
+    Promise.all([
+      $api.admin.getTranslationLogs(PER_PAGE),
+      $api.admin.getTranslationsCount(),
+      $api.admin.getTranslationsCount('bur2ru'),
+      $api.admin.getTranslationsCount('ru2bur'),
+    ]),
+  );
+  logs.value = data.value?.[0] || null;
+  allTranslationsNumber = data.value?.[1] || 0;
+  ru2burTranslationsNumber = data.value?.[2] || 0;
+  bur2ruTranslationsNumber = data.value?.[3] || 0;
+
+  const lastPage = Math.ceil(allTranslationsNumber / PER_PAGE);
+
+  async function changePage(page: number) {
+    try {
+      isLoading.value = true;
+      history.replaceState(null, '', `?page=${page}`);
+      currentPage.value = page;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+
+      logs.value = await $api.admin.getTranslationLogs(
+        PER_PAGE,
+        page * PER_PAGE - PER_PAGE,
+      );
+    } catch (e) {
+      console.error(e);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  const activeRequestsForIgnoring = new Set();
 
   async function ignoreLogItem(id: number) {
+    if (activeRequestsForIgnoring.has(id)) return;
+
+    activeRequestsForIgnoring.add(id);
     await $api.admin.ignoreTranslationLog(id);
     logs.value = await $api.admin.getTranslationLogs();
   }
