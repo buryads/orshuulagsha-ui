cbfb54dc3e5ea4e71cf0d4a3a857cdc1d12dbec1
savayer <sava0094@gmail.com>
2024-02-10 17:42:56 +0400

    (wip) creating filters

--- components/UI/DropdownItem.vue
+++ components/UI/DropdownItem.vue
@@ -4,6 +4,7 @@
       :class="[
         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
         'block px-4 py-2 text-sm',
+        className,
       ]"
     >
       <slot />
@@ -13,4 +14,6 @@
 
 <script setup lang="ts">
   import { MenuItem } from '@headlessui/vue';
+
+  defineProps(['className']);
 </script>
