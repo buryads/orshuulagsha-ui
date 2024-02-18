3aa4ef727a15eeb4204ce4261c18da9821cdba8d
savayer <sava0094@gmail.com>
2024-02-18 21:09:08 +0400

    updated images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap',
         },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ],
     },
     /* page transition doesn't work https://github.com/nuxt/nuxt/issues/13350 */
@@ -34,16 +35,36 @@ export default defineNuxtConfig({
     registerType: 'autoUpdate',
     manifest: {
       name: 'Buryad Orod Toli',
-      short_name: 'Buryad Orod Toli',
+      short_name: 'Buryad Toli',
       theme_color: '#ffffff',
       icons: [
         {
-          src: '/images/logo-192.png',
+          src: '/icons/48.png',
+          sizes: '48x48',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/72.png',
+          sizes: '72x72',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/96.png',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: '/images/logo-512.png',
+          src: '/icons/256.png',
+          sizes: '256x256',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/512.png',
           sizes: '512x512',
           type: 'image/png',
         },
